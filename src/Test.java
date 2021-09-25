eb5bd34e53addb07afcc0b2e2af990a837fc0448
vladthetree <50account!>
2021-09-25 19:55:14 +0200

    test2

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,8 @@ public class Test {
 
     public static void main (String [] args){
         System.out.println("Test123");
+        for (int i = 0; i<10; i++){
+            System.out.println("Zahl "+(i+1));
+        }
     }
 }
