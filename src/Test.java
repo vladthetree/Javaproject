9805ccac0fcc67165f86f3a4f09740b9da2a54d5
Vladyslav <vladysspam@gmx.de>
2021-11-01 18:52:29 +0100

    win11

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main (String [] args){
-        System.out.println("Test123");
+        System.out.println("Test123WithWin11");
         for (int i = 0; i<10; i++){
             System.out.println("Zahl "+(i+1));
         }
