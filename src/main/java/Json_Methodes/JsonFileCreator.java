d89a91da0862be47dc3ee1897534c5ad5306981a
Vladyslav <vladysspam@gmx.de>
2021-12-10 00:54:22 +0100

    added possibilit to add single Values to Json

--- src/main/java/Json_Methodes/JsonFileCreator.java
+++ src/main/java/Json_Methodes/JsonFileCreator.java
@@ -19,13 +19,20 @@ public class JsonFileCreator {
 
         JSONObject jsonObject = new JSONObject();
 
-        Map<String, Integer> values = new HashMap<>();
-        values.put("Alpha", 2);
-        values.put("Betha", 3);
-        values.put("Gamma", 4);
-        values.put("Omega", 5);
-        values.put("Gaga", 8);
-        giveNamesToObjects(jsonObject, values);
+        Map<String, Integer> objectValues = new HashMap<>();
+        objectValues.put("Alpha", 2);
+        objectValues.put("Betha", 3);
+        objectValues.put("Gamma", 4);
+        objectValues.put("Omega", 5);
+        objectValues.put("Gaga", 8);
+
+        Map<String,String>singleValues = new HashMap<>();
+        singleValues.put("asOfDate","99.99.9999");
+        singleValues.put("Customername","Franzo");
+
+
+        createManyJsonArrays(jsonObject, objectValues);
+        createSingleValues(jsonObject,singleValues);
 
         PrintWriter pw = new PrintWriter("G:\\Workload\\JSONExample.json");
         pw.write(jsonObject.toJSONString());
@@ -34,24 +41,26 @@ public class JsonFileCreator {
         pw.close();
     }
 
-    private static void createJsonObject(JSONObject jsonObject, String suffix, int amount) {
+    private static void createJsonObject(JSONObject jsonObject, String declaration, int amount) {
         Map<String, String> linkedHashMap = new LinkedHashMap<>(amount);
 
-
         for (int i = 0; i < amount; i++) {
-            linkedHashMap.put("name_" + i + "_" + suffix, "value_" + i + "_" + suffix); // i wird iteriert weil, lHashmap unique ist
+            linkedHashMap.put("name_" + (i+1) + "_" + declaration, "value_" + (i+1) + "_" + declaration); // i wird iteriert weil, lHashmap unique ist
         }
         JSONArray jsonArray = new JSONArray();
         jsonArray.add(linkedHashMap);
-        jsonObject.put(suffix, jsonArray);
+        jsonObject.put(declaration, jsonArray);
 
     }
 
-
-    private static void giveNamesToObjects(JSONObject jsonObject, Map<String, Integer> map) {
+    private static void createManyJsonArrays(JSONObject jsonObject, Map<String, Integer> map) {
         for (Map.Entry<String, Integer> set : map.entrySet()) {
             createJsonObject(jsonObject, set.getKey(), set.getValue());
         }
+    }
+
+    private static void createSingleValues(JSONObject jsonObject,Map<String,String>map){
+        jsonObject.putAll(map);
 
     }
 }
