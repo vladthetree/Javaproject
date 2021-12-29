d8db53713c1e87d490d48c0a1710148b387d97b8
vladthetree <50account!>
2021-12-29 18:59:32 +0100

    start to add string to Mongo

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,14 +1,9 @@
 package main;
 
 
-import DB.Mongo.MongoConnection;
 import Toolbox.csv_Methodes.CsvConverter;
-import Toolbox.json_Methodes.JsonConverter;
-import com.mongodb.client.MongoCollection;
 import java.util.List;
 import java.util.Map;
-import org.bson.Document;
-import org.json.simple.JSONObject;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories(basePackages = "DB.Mongo.Configuration")
@@ -21,16 +16,22 @@ public class main {
 
     List<String[]> strings = CsvConverter.csvToStringArrayList(path).stream().skip(4).toList();
     Map<String, String> stringStringMap = CsvConverter.mapTwoCsvColumn(strings, 1, 3);
-//        JSONObject jsonObject = JsonCreator.stringMaptoJsonObject(stringStringMap);
-    JSONObject jsonObject1 = new JSONObject();
-    jsonObject1.put("name", "test1");
-    jsonObject1.put("id", "123");
-    jsonObject1.put("map", stringStringMap); // TODO DAS ALS METHODE UMSETZEN
+////        JSONObject jsonObject = JsonCreator.stringMaptoJsonObject(stringStringMap);
+//    JSONObject jsonObject1 = new JSONObject();
+//    jsonObject1.put("name", "test1");
+//    jsonObject1.put("id", "123");
+//    jsonObject1.put("map", stringStringMap); // TODO DAS ALS METHODE UMSETZEN
+//
+//    Document bsonFile = JsonConverter.jsonToBson(jsonObject1);
+//
+//    MongoCollection col = MongoConnection.getCollectionFromDatabase("uat", "bsonObject");
+//    col.insertOne(bsonFile);
 
-    Document bsonFile = JsonConverter.jsonToBson(jsonObject1);
 
-    MongoCollection col = MongoConnection.getCollectionFromDatabase("uat", "bsonObject");
-    col.insertOne(bsonFile);
+
+    
+
+
 
 
   }
